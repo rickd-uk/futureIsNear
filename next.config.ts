48df2a747a35e6ce27d776621b954eaa1fe5097d
rickd-uk <78909973+rickd-uk@users.noreply.github.com>
2025-12-01 14:09:39 +0900

    Remove deprecated serverRuntimeConfig

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  serverRuntimeConfig: {
-    port: process.env.PORT || 5000,
-  },
 };
 
 export default nextConfig;
